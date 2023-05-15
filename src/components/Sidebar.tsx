3ac9ce3eeda67966119816885a4dd4958ee50f9c
Grellheist <grellheist@tutanota.com>
2023-05-15 10:36:14 -0300

    Fixed some padding issues

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -10,8 +10,8 @@ import { HiDotsCircleHorizontal, HiDotsHorizontal } from "react-icons/hi"
 export default function Sidebar() {
     return (
         <div className="hidden sm:flex flex-col p-2 xl:items-start fixed h-full">
-            <div className="hoverEffect p-0 hover:bg-gray-800 xl:px-1 pt-4">
-                <BsTwitter className="h-8 w-7" />
+            <div className="hoverEffect xl:mt-0.5">
+                <BsTwitter className="w-7 h-8" />
             </div>
 
             <div className="mt-4 mb-2.5 xl:items-start">
--- src/components/SidebarMenuItem.tsx
+++ src/components/SidebarMenuItem.tsx
@@ -10,7 +10,7 @@ type SidebarMenuItemProps = {
 export default function SidebarMenuItem({ text, Icon, active }: SidebarMenuItemProps) {
     return (
         <div className='hoverEffect flex items-center justify-center xl:justify-start text-lg space-x-3 '>
-            <Icon className='h-7' />
+            <Icon className='h-7 w-5' />
             <span className={`${active && "font-bold"} hidden xl:inline`}>{text}</span>
         </div>
     )
