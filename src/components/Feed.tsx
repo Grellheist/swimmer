5f724203286defda8754c3136589a346eb270ff9
grellheist <grellheist@tutanota.com>
2023-05-20 02:18:19 -0300

    Made the margins a little better and closer to the real twitter

--- src/components/Feed.tsx
+++ src/components/Feed.tsx
@@ -24,7 +24,7 @@ export default function Feed() {
         },
     ];
     return (
-        <div className='xl:ml-[370px] border-l border-gray-600 border-r xl:min-w-[576px] sm:ml-[73px] flex-grow max-w-xl'>
+        <div className='xl:ml-[330px] border-l border-gray-600 border-r xl:min-w-[620px] sm:ml-[73px] flex-grow max-w-xl'>
             <div className='flex py-2 px-3 sticky top-0 z-50 bg-black border-b border-gray-600'>
                 <h2 className='text-lg sm:text-xl font-bold cursor-pointer flex items-center justify-center'>Home</h2>
                 <div className='hoverEffect flex items-center justify-center px-0 ml-auto w-9 h-9'>
