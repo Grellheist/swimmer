ab5e007b35e61013e9a7c406ff96704cad55a670
grellheist <grellheist@tutanota.com>
2023-05-21 14:23:32 -0300

    Fixed some responsiveness issues

--- src/components/SidebarMenuItem.tsx
+++ src/components/SidebarMenuItem.tsx
@@ -9,7 +9,7 @@ type SidebarMenuItemProps = {
 
 export default function SidebarMenuItem({ text, Icon, active }: SidebarMenuItemProps) {
     return (
-        <div className='hoverEffect flex items-center justify-center xl:justify-start space-x-3 '>
+        <div className='hoverEffect flex items-center justify-center xl:justify-start space-x-3'>
             <Icon className='mr-3 text-[28px]' />
             <span className={`${active && "font-semibold"} hidden xl:inline text-[22px]`}>{text}</span>
         </div>
