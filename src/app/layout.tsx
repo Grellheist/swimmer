96518f69866caf3d13c01fa5a113ff09960875f1
Grellheist <grellheist@tutanota.com>
2023-05-23 17:18:24 -0300

    Added smooth scrolling to the html tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
     children: React.ReactNode
 }) {
     return (
-        <html lang="en">
+        <html lang="en" className='scroll-smooth'>
             <body className={`${inter.className} flex min-h-screen mx-auto`}>
                 <Sidebar />
                 <Feed />
