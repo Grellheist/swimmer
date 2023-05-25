08b883d4b060d4ed0ab0903760e544a025262a01
Grellheist <grellheist@tutanota.com>
2023-05-25 10:37:31 -0300

    Fixed the name in the header to not be dynamic anymore

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -2,11 +2,8 @@
 import { HiSparkles } from 'react-icons/hi'
 import Input from '@/components/Input'
 import Post from '@/components/Post'
-import { usePathname } from 'next/navigation';
 
 export default function Home() {
-    const pathName = usePathname().substring(1)
-    const formattedName = `${pathName.charAt(0).toUpperCase()}${pathName.slice(1)}`
 
     const posts = [
         {
@@ -58,7 +55,7 @@ export default function Home() {
     return (
         <div className='sm:mr-auto xl:ml-[330px] border-l border-gray-600 border-r xl:min-w-[630px] sm:ml-[73px] flex-grow max-w-xl'>
             <div className='flex py-2 px-3 sticky top-0 z-50 bg-black border-b border-gray-600 bg-opacity-70 backdrop-blur'>
-                <h2 className='text-lg sm:text-xl font-bold cursor-pointer flex items-center justify-center'>{formattedName}</h2>
+                <h2 className='text-lg sm:text-xl kont-bold cursor-pointer flex items-center justify-center'>Home</h2>
                 <div className='hoverEffect flex items-center justify-center px-0 ml-auto w-9 h-9'>
                     <HiSparkles className='h-7' />
                 </div>
