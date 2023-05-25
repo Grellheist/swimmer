045545073480ae40e8009a6a9f59be9f958c1bcd
grellheist <grellheist@tutanota.com>
2023-05-24 21:39:49 -0300

    Added next auth and removed some useless dependencies

---
+++ firebase.js
@@ -0,0 +1,17 @@
+import { initializeApp, getApp, getApps } from "firebase/app";
+import { getFirestore } from "firebase/firestore"
+import { getStorage } from "firebase/storage"
+
+const firebaseConfig = {
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: "whisker-67c7d.firebaseapp.com",
+    projectId: "whisker-67c7d",
+    storageBucket: "whisker-67c7d.appspot.com",
+    messagingSenderId: "251803151312",
+    appId: "1:251803151312:web:61bbc24a802f5512ada7d5"
+};
+
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+const db = getFirestore();
+const storage = getStorage();
+export {app, db, storage}
