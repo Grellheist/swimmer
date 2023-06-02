f2127c85594958f3f8096ad7a51aba7b216fcdff
grellheist <grellheist@tutanota.com>
2023-06-02 18:47:44 -0300

    nvm that broke things

--- src/components/SidebarMenuItem.tsx
+++ src/components/SidebarMenuItem.tsx
@@ -1,14 +1,11 @@
 "use client"
-import { useUser } from "@clerk/nextjs"
 import { SidebarMenuItemProps } from "./types"
 import Link from 'next/link'
 import { usePathname } from "next/navigation"
 
 export default function SidebarMenuItem({ text, Icon }: SidebarMenuItemProps) {
-    const { user } = useUser()
     return (
-        // @ts-ignore
-        <Link href={`${text.toLowerCase() === 'profile'}` ? `/profile/${user.id}` : `${text.toLowerCase()}`} className='hoverEffect flex items-center justify-center xl:justify-start space-x-3' >
+        <Link href={`/${text.toLowerCase()}`} className='hoverEffect flex items-center justify-center xl:justify-start space-x-3' >
             <Icon className='mr-3 text-[28px]' />
             <span className={`${usePathname() === `/${text.toLowerCase()}` ? "font-semibold" : ""} hidden xl:inline text-[22px]`}>{text}</span>
         </Link >
