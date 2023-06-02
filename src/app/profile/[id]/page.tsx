abd68ac295e66ce7631de541868183a199c959d5
grellheist <grellheist@tutanota.com>
2023-06-02 19:16:15 -0300

    Started user profile

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -1,13 +1,27 @@
 "use client"
 import { useUser } from "@clerk/nextjs"
 import Image from "next/image"
+import { MdBrokenImage } from "react-icons/md"
 
 export default async function Profile() {
     const { user } = useUser()
 
     return (
         <div>
-            {user?.fullName} profile
+            <div>{user?.fullName}</div>
+            <div>@{user?.username}</div>
+            {user && user.imageUrl ? (
+                <Image
+                    src={user.imageUrl}
+                    alt="user image"
+                    className="rounded-full xl:mr-2 w-11 h-11"
+                    width="150"
+                    height="150"
+                />
+            ) : (
+                <MdBrokenImage className="h-7 w-7" />
+            )}
+
         </div>
     )
 }
