2832996ebabdb24cb689a3402cba26e94fe30d1a
grellheist <grellheist@tutanota.com>
2023-06-04 19:54:28 -0300

    Finished more page

--- src/app/more/page.tsx
+++ src/app/more/page.tsx
@@ -1,5 +1,14 @@
+"use client"
+import YouTube from "react-youtube"
+
 export default function More() {
     return (
-        <div></div>
+        <div>
+            <div className="text-lg ml-6 mt-5 font-semibold">Why do you want more? Are you not entertained?</div>
+            <div className="text-md ml-6 mt-2">No? Here then, watch this:</div>
+            <div className="items-center justify-center flex mt-9">
+                <YouTube opts={{ height: "300", width: "600" }} videoId="XD7UAgb9JIo" />
+            </div>
+        </div>
     )
 }
