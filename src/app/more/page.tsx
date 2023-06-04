f754a7d592cd6832b9200ce94346245597e3e3eb
grellheist <grellheist@tutanota.com>
2023-06-04 20:16:31 -0300

    yeee

--- src/app/more/page.tsx
+++ src/app/more/page.tsx
@@ -5,7 +5,7 @@ export default function More() {
     return (
         <div>
             <div className="text-lg ml-6 mt-5 font-semibold">Why do you want more? Are you not entertained?</div>
-            <div className="text-md ml-6 mt-2 font-semibold">No? Here then, watch <span className="font-bold font-blue-500">this</span>:</div>
+            <div className="text-md ml-6 mt-2 font-semibold">No? Here then, watch <span className="font-bold text-blue-500">this</span>:</div>
             <div className="items-center justify-center flex mt-9">
                 <YouTube opts={{ height: "300", width: "600" }} videoId="XD7UAgb9JIo" />
             </div>
