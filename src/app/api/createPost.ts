10a21a9a344c430e17b88d5cc27adcffd61b8c49
Grellheist <grellheist@tutanota.com>
2023-06-05 15:28:08 -0300

    Trying new stuff

--- src/app/api/createPost.ts
+++ src/app/api/createPost.ts
@@ -3,17 +3,15 @@ import { NextApiRequest, NextApiResponse } from "next";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     if (req.method === "POST") {
-        const { textValue } = req.body;
-
+        const post = JSON.parse(req.body);
         try {
-            const entry = await prisma.post.create({
+            const data = await prisma.post.create({
                 data: {
                     authorId: "hahah",
-                    content: textValue,
                 },
             });
 
-            res.status(201).json(entry);
+            res.status(200).json(data);
         } catch (error) {
             console.error(error);
             res.status(500).json({ error: "Failed to create entry" });
