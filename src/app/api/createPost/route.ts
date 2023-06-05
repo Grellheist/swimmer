a1e19b0a295fecc4acdcdead3bad1f5626b3ec51
grellheist <grellheist@tutanota.com>
2023-06-05 19:24:51 -0300

    Ok this should work

--- src/app/api/createPost/route.ts
+++ src/app/api/createPost/route.ts
@@ -1,6 +1,5 @@
 import { prisma } from "@/lib/prisma";
 import { NextResponse } from "next/server";
-import Router from "next/router";
 
 export async function POST(request: Request) {
     const body = await request.json()
@@ -11,6 +10,6 @@ export async function POST(request: Request) {
             content,
         },
     });
-    Router.reload()
+    NextResponse.redirect("/home")
     return NextResponse.json(data)
 }
