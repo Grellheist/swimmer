d2930a78698a86178344f0b901fcb34cdc1c74da
Grellheist <grellheist@tutanota.com>
2023-06-05 14:08:02 -0300

    Maybe this?

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -21,7 +21,7 @@ export default function Input() {
 
     const handleMeow = async () => {
         try {
-            const response = await fetch("../app/api/createEntry", {
+            const response = await fetch("../app/api/createEntry.ts", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json",
