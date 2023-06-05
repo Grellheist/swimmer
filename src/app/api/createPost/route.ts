30c2bfe9271c12ae6a49d4e11caaf9b7cfc8f559
Grellheist <grellheist@tutanota.com>
2023-06-05 15:42:51 -0300

    ok dude

--- src/app/api/createPost/route.ts
+++ src/app/api/createPost/route.ts
@@ -2,7 +2,6 @@ import { prisma } from "@/lib/prisma";
 import { NextResponse } from "next/server";
 
 export async function POST(request: Request) {
-    const { authorId } = request.body
     const data = await prisma.post.create({
         data: {
             authorId: "hahah",
