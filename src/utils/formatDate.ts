14bffd5358d9bf5cdce38038c6d1ddaf5fa2f4a6
grellheist <grellheist@tutanota.com>
2023-06-10 16:28:17 -0300

    Oopsie

--- src/utils/formatDate.ts
+++ src/utils/formatDate.ts
@@ -1,4 +1,4 @@
-function formatTwitterDate(dateString: string) {
+export default function formatDate(dateString: string) {
     const date = new Date(dateString);
     const currentDate = new Date();
 
