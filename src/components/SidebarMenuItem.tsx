d44d1f42432a10e14af7feb0ffc73cad9a043698
grellheist <grellheist@tutanota.com>
2023-06-10 15:35:54 -0300

    ok this fixes things I think

--- src/components/SidebarMenuItem.tsx
+++ src/components/SidebarMenuItem.tsx
@@ -4,12 +4,10 @@ import Link from 'next/link'
 import { usePathname } from "next/navigation"
 
 export default function SidebarMenuItem({ text }: SidebarMenuItemProps, { Icon }: SidebarMenuItemProps) {
-    const currentPath = usePathname()
-    const isProfileRoute = currentPath.includes('/profile')
     return (
-        <Link href={`/${text.toLowerCase()}`} className={`hoverEffect flex items-center justify-center xl:justify-start space-x-3 ${isProfileRoute ? 'font-semibold' : ''}`}>
+        <Link href={`/${text.toLowerCase()}`} className='hoverEffect flex items-center justify-center xl:justify-start space-x-3' >
             <Icon className='mr-3 text-[28px]' />
-            <span className="hidden xl:inline text-[22px]">{text}</span>
-        </Link>
+            <span className={`${usePathname() === `/${text.toLowerCase()}` ? "font-semibold" : ""} hidden xl:inline text-[22px]`}>{text}</span>
+        </Link >
     )
 }
