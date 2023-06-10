db1f44126cddead053ab8adb86dabb5e5b827f54
grellheist <grellheist@tutanota.com>
2023-06-10 15:29:59 -0300

    Improved some stuff in the sidebar

--- src/components/SidebarMenuItem.tsx
+++ src/components/SidebarMenuItem.tsx
@@ -1,13 +1,17 @@
 "use client"
 import { SidebarMenuItemProps } from "./types"
 import Link from 'next/link'
-import { usePathname } from "next/navigation"
+import { useRouter } from 'next/router'
 
-export default function SidebarMenuItem({ text, Icon }: SidebarMenuItemProps) {
+export default function SidebarMenuItem({ text }: SidebarMenuItemProps, { Icon }: SidebarMenuItemProps) {
+    const router = useRouter()
+    const isActive = router.asPath.startsWith('/profile') || router.asPath.match(text.toLowerCase())
+
     return (
         <Link href={`/${text.toLowerCase()}`} className='hoverEffect flex items-center justify-center xl:justify-start space-x-3' >
             <Icon className='mr-3 text-[28px]' />
-            <span className={`${usePathname() === `/${text.toLowerCase()}` ? "font-semibold" : ""} hidden xl:inline text-[22px]`}>{text}</span>
-        </Link >
+            <span className={`${isActive ? "font-semibold" : ""} hidden xl:inline text-[22px]`}>{text}</span>
+        </Link>
     )
 }
+
