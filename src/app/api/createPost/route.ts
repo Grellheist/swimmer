859999b72e47683cdf0654d21496d799ab3c80ac
Grellheist <grellheist@tutanota.com>
2023-06-12 12:01:19 -0300

    made an oopsie

--- src/app/api/createPost/route.ts
+++ src/app/api/createPost/route.ts
@@ -3,11 +3,12 @@ import { NextResponse } from "next/server";
 
 export async function POST(request: Request) {
     const body = await request.json()
-    const { content, authorId } = body
+    const { content, authorId, imgUrl } = body
     const data = await prisma.post.create({
         data: {
             authorId,
             content,
+            imgUrl
         },
     });
     return NextResponse.json(data)
