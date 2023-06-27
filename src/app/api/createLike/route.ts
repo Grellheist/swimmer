079cbd770acca7a815e4a2ae679faac855a548d0
Grellheist <grellheist@tutanota.com>
2023-06-27 10:43:42 -0300

    Testing new api route for likes

--- src/app/api/createLike/route.ts
+++ src/app/api/createLike/route.ts
@@ -4,11 +4,26 @@ import { NextResponse } from "next/server";
 export async function POST(request: Request) {
     const body = await request.json()
     const { postId, userId } = body
-    const data = await prisma.like.create({
-        data: {
+    const existingLike = await prisma.like.findFirst({
+        where: {
             postId,
             userId
         }
     })
-    return NextResponse.json(data)
+    if (!existingLike) {
+        const data = await prisma.like.create({
+            data: {
+                postId,
+                userId
+            }
+        })
+        return NextResponse.json(data)
+    } else {
+        const data = await prisma.like.delete({
+            where: {
+                id: existingLike.id
+            }
+        })
+        return NextResponse.json(data)
+    }
 }
