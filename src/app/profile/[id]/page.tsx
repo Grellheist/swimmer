b2907dbc70cba76edf0cf9d829f882933d11884d
Grellheist <grellheist@tutanota.com>
2023-06-29 17:10:20 -0300

    oh ok i get it

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -1,18 +1,16 @@
-"use client"
-import { useUser } from "@clerk/nextjs"
 import Image from "next/image"
 import getUser from "@/utils/getUser"
 
 export default async function Profile({ params }: { params: { id: string } }) {
-    const { user } = useUser()
+    const user = await getUser(params.id)
     if (!user) return <div>404</div>
 
     return (
         <div>
-            <div>{user?.fullName}</div>
-            <div>@{user?.username}</div>
+            <div>{user.name}</div>
+            <div>@{user.username}</div>
                 <Image
-                    src={user.imageUrl}
+                    src={user.userImg}
                     alt="user image"
                     className="rounded-full xl:mr-2 w-11 h-11"
                     width="150"
