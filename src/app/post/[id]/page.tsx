13bab65e702981530bff2f999638f8c124fe10ba
Grellheist <grellheist@tutanota.com>
2023-06-29 16:00:32 -0300

    Improved post page

--- src/app/post/[id]/page.tsx
+++ src/app/post/[id]/page.tsx
@@ -1,23 +1,42 @@
 import getUser from "@/utils/getUser"
 import getSinglePost from "@/utils/getSinglePost"
 import getComments from "@/utils/getComments"
+import checkIfUserLiked from "@/utils/checkIfUserLiked"
 import Post from "@/components/Post"
 import Comment from "@/components/Comment"
 import React from "react"
+import { currentUser } from "@clerk/nextjs/dist/server-helpers.server"
+
+async function getCurrentUser() {
+    const user = await currentUser()
+    if (!user) {
+        return null
+    }
+    return user.id
+}
 
 export default async function page({ params }: { params: { id: string } }) {
     const post = await getSinglePost(params.id)
     const comments = await getComments(params.id)
+    const currentUserId = await getCurrentUser()
+
+    if (!currentUserId) {
+        throw new Error("Not logged in")
+    }
+
     if (!comments) {
         throw new Error("Something went wrong!")
     }
+
     if (!post) {
         return (
             <div>404</div>
         )
     }
+
     const user = await getUser(post.authorId)
-    const postProps = { ...post, ...user }
+    const hasLiked = await checkIfUserLiked(currentUserId, post.id)
+    const postProps = { ...post, ...user, hasLiked }
 
     const fetchUserInformation = async (authorId: string) => {
         const { userImg, username, name } = await getUser(authorId)
