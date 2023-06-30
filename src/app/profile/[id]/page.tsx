f02967e22b144032787e97dce5ad2ad48a189854
Grellheist <grellheist@tutanota.com>
2023-06-30 10:55:27 -0300

    finished profile page

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -4,6 +4,7 @@ import { currentUser } from "@clerk/nextjs"
 import checkIfUserLiked from "@/utils/checkIfUserLiked"
 import getUserPosts from "@/utils/getUserPosts"
 import Post from "@/components/Post"
+import Head from "next/head"
 
 async function getCurrentUser() {
     const user = await currentUser()
@@ -49,19 +50,27 @@ export default async function Profile({ params }: { params: { id: string } }) {
     if (!user) return <div>404</div>
 
     return (
-        <div>
-            <div>{user.name}</div>
-            <div>@{user.username}</div>
+        <>
+            <Head>
+                <title>{user.username}</title>
+            </Head>
+            <div className="relative h-36 bg-slate-600">
                 <Image
                     src={user.userImg}
-                    alt="user image"
-                    className="rounded-full xl:mr-2 w-11 h-11"
-                    width="150"
-                    height="150"
+                    alt={`${user.username}'s profile pic`}
+                    width={128}
+                    height={128}
+                    className="absolute bottom-0 left-0 -mb-[64px] ml-4 rounded-full border-4 border-black bg-black"
                 />
-                {postsWithUserInformation.map((post) => (
-                    <Post key={post.id} post={post}/>
-                ))}
-        </div>
+            </div>
+            <div className="h-[64px]"></div>
+            <div className="pl-4 pt-4 text-2xl font-bold">{user.name}</div>
+            <div className="pl-4 pb-4 text-md text-gray-500">{`@${user.username}`}</div>
+            <div className="w-full border-b border-gray-600" />
+            {postsWithUserInformation.map((post) => (
+                <Post key={post.id} post={post} />
+            ))
+            }
+        </>
     )
 }
