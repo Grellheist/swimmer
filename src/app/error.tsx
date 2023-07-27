ebff44e49094c3f2bdbbc8efe495f08eb0b08dac
Grellheist <grellheist@tutanota.com>
2023-07-27 08:59:16 -0300

    changed the error page because of a common bug

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -7,9 +7,11 @@ export default function Error({ error, reset }: { error: Error; reset: () => voi
         console.log(error);
     }, [error]);
     return (
-        <div>
-            <h2>Something went wrong!</h2>
-            <button onClick={() => reset()}>Try again</button>
+        <div className="bg-black h-screen flex flex-col justify-center items-center z-50">
+            <h2 className="text-gray-200 text-lg">Something went wrong!</h2>
+            <h2 className="text-gray-200 text-lg">Try reloading the page <span className="text-blue-500">(maybe CTRL + F5?)</span>.</h2>
+            <h2 className="text-gray-200 text-lg">Or click the button:</h2>
+            <button className="text-gray-200 hover:text-blue-500 text-lg" onClick={() => reset()}>Try again</button>
         </div>
     )
 }
